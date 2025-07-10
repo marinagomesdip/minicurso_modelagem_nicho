c49408c8ce88a2c2454dde12d75752d7d27cd0c4
Marina <gomes.mari.95@gmail.com>
2025-07-10 16:29:17 -0300

    atualizando scprit

---
+++ Scripts/Prática_1.R
@@ -0,0 +1,5 @@
+#Minicurso Como a tecnologia pode auxiliar a entender a distribuição das espécies?
+#Marina Morim Gomes & Durval Silva Santos 2025
+#gomes.mari.95@gmail.com
+
+#instalando os pacotes requeridos -------------------------------------
